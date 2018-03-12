ffd91cc406e5a534e27292c46f665134053eaccb
Dylan <johnston94@gmail.com>
2018-03-12 13:50:55 -0500

    change button classes

--- wp-content/themes/kma-slim/template-parts/sidebars/get-involved-sidebar.php
+++ wp-content/themes/kma-slim/template-parts/sidebars/get-involved-sidebar.php
@@ -1,22 +1,22 @@
 <h3>Get Involved</h3>
 <p>The Bay County Public Library Foundation exists because of the support of people like you. If your interested in volunteering or contributing, click below for  more information.</p>
-<a class="button is-rounded is-secondary"
-href="<?= $event['tickets']; ?>"
+<a class="button is-rounded is-primary is-outlined"
+href=""
 >
     Attend an Event
 </a>
-<a class="button is-rounded is-secondary"
-href="<?= $event['tickets']; ?>"
+<a class="button is-rounded is-primary is-outlined"
+href="#"
 >
     Sponsor an Event
 </a>
-<a class="button is-rounded is-secondary"
-href="<?= $event['tickets']; ?>"
+<a class="button is-rounded is-primary is-outlined"
+href="#"
 >
     Volunteer an Event
 </a>
-<a class="button is-rounded is-secondary"
-href="<?= $event['tickets']; ?>"
+<a class="button is-rounded is-primary is-outlined"
+href="/donate-support/make-a-donation/"
 >
     Make a Donation
 </a>
