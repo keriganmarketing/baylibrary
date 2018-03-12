3c6d87b1ecb11dd5b4b9ec5d129fbd4ed7996792
Dylan <johnston94@gmail.com>
2018-03-12 13:37:01 -0500

    Change sidebar

--- wp-content/themes/kma-slim/template-parts/sidebars/get-involved-sidebar.php
+++ wp-content/themes/kma-slim/template-parts/sidebars/get-involved-sidebar.php
@@ -1,1 +1,23 @@
 <h3>Get Involved</h3>
+<p>The Bay County Public Library Foundation exists because of the support of people like you. If your interested in volunteering or contributing, click below for  more information.</p>
+<a class="button is-rounded is-secondary"
+href="<?= $event['tickets']; ?>"
+>
+    Attend an Event
+</a>
+<a class="button is-rounded is-secondary"
+href="<?= $event['tickets']; ?>"
+>
+    Sponsor an Event
+</a>
+<a class="button is-rounded is-secondary"
+href="<?= $event['tickets']; ?>"
+>
+    Volunteer an Event
+</a>
+<a class="button is-rounded is-secondary"
+href="<?= $event['tickets']; ?>"
+>
+    Make a Donation
+</a>
+    </div>
