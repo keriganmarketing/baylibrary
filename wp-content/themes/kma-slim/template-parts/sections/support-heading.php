5b88611896942e28aefc324549801566be1a3a2f
programmer-man <bryan@kerigan.com>
2018-03-14 17:45:45 -0400

    fix support header styles

--- wp-content/themes/kma-slim/template-parts/sections/support-heading.php
+++ wp-content/themes/kma-slim/template-parts/sections/support-heading.php
@@ -1,9 +1,10 @@
 <div class="sticky-header-pad support"></div>
-<section class="support-header">
+<section class="support-header"></section>
+<section class="support-title">
     <div class="container">
-        <h1 class="title is-3"><?php echo $headline; ?></h1>
-        <?php echo ($subhead!='' ? '<p class="subtitle is-white">'.$subhead.'</p>' : null); ?>
-        <?php if ( 'post' === get_post_type() ) : ?>
+        <h1 class="title is-1"><?php echo $headline; ?></h1>
+        <?php echo($subhead != '' ? '<p class="subtitle is-white">' . $subhead . '</p>' : null); ?>
+        <?php if ('post' === get_post_type()) : ?>
             <div class="entry-meta">
                 <?php //kmaslim_posted_on(); ?>
             </div>
