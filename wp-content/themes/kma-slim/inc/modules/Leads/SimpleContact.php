ce5f02d56fa20b0f9dc95b97c053e74e1211c2dd
programmer-man <bryan@kerigan.com>
2018-03-22 11:13:33 -0400

    admin email test

--- wp-content/themes/kma-slim/inc/modules/Leads/SimpleContact.php
+++ wp-content/themes/kma-slim/inc/modules/Leads/SimpleContact.php
@@ -13,7 +13,7 @@ class SimpleContact extends Leads
             ]
         );
         parent::set('postType', 'Contact Submission');
-        parent::set('adminEmail', 'webmaster@bcplfoundation.org');
+        parent::set('adminEmail', 'bryan@kerigan.com');
     }
 
     protected function showForm()
