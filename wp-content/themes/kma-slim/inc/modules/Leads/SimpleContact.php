cf6d1dfed3f3807b4a1c127ed8288805141a6d92
programmer-man <bryan@kerigan.com>
2018-03-22 11:18:04 -0400

    move to production

--- wp-content/themes/kma-slim/inc/modules/Leads/SimpleContact.php
+++ wp-content/themes/kma-slim/inc/modules/Leads/SimpleContact.php
@@ -13,7 +13,7 @@ class SimpleContact extends Leads
             ]
         );
         parent::set('postType', 'Contact Submission');
-        parent::set('adminEmail', 'bryan@kerigan.com');
+        parent::set('adminEmail', 'info@bcplfoundation.org');
     }
 
     protected function showForm()
